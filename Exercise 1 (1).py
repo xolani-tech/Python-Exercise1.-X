f0cf51085127ebd2c94f3534e4ea954b391a8b58
xolani-tech <jonathanmicah23@gmal.com>
2024-11-08 10:19:47 +0200

    anything

---
+++ Exercise 1 (1).py
@@ -0,0 +1,27 @@
+# Question 1: Variable Assignment and String Manipulation
+
+# TODO: Ask the user for their name and store it in a variable
+
+# TODO: Ask the user for their age and store it in a variable
+
+# TODO: Print a greeting using the name and age variables
+
+#------------------------------------------------------------------------------------
+# Question 2: Integer Operations
+
+# TODO: Ask the user for the length of a rectangle and store it as an integer
+
+# TODO: Ask the user for the width of a rectangle and store it as an integer
+
+# TODO: Calculate the area of the rectangle
+
+# TODO: Print the result
+
+#------------------------------------------------------------------------------------
+# Question 3: Working with Floats
+
+# TODO: Ask the user for a temperature in Celsius and store it as a float
+
+# TODO: Convert the temperature to Fahrenheit using the formula: (C * 9/5) + 32
+
+# TODO: Print the result rounded to two decimal places
